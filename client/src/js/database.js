d2182445f1cf23d845db56a36e8e79fcb6813644
Tyler Mifflin <you@example.com>
2023-07-05 16:35:53 -0600

    updated the putDd function to use transaction and objectstore

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,12 +14,16 @@ const initdb = async () =>
   });
 
 // export a function to update the database
-export const putDb = async (content) => {
-  console.log('content added to database' + content);
+export const putDb = async (content) => {  
 // create a connection to the database
   const db = await openDB('jate', 1);
+//create a transaction to update the database
+  const tx = db.transaction('jate', 'readwrite');
+// create a store to update the database
+  const store = tx.objectStore('jate');
 // update the content to the database
   await db.put('jate', content);
+  console.log('content added to database' + content);
 };
 
 // export a function to get the content from the database
