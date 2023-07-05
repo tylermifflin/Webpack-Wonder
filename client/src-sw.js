a95ca5334b888c5f3f56dc342511c3681fbbbcab
Tyler Mifflin <you@example.com>
2023-07-05 15:43:08 -0600

    implement assest caching function set up

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,21 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// set up asset cache
+registerRoute(
+  ({ request }) => request.destination === 'script' ||
+    request.destination === 'style' ||
+    request.destination === 'font' ||
+    request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  }),
+);
