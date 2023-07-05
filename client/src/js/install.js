ae3fb2c6013aa61823d79828c7d5fc2f6073cd64
Tyler Mifflin <you@example.com>
2023-07-05 15:22:49 -0600

    setting up the event listener to download the app

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,14 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent Chrome <= 67 from automatically showing the prompt
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Update UI notify the user they can add to home screen
+    butInstall.removeAttribute('hidden');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
